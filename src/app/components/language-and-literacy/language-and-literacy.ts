59ad098b16d763ababca426e18aca2db44fef60d
AMATYA <Jeny.AMATYA@qed.qld.gov.au>
2026-04-20 13:44:07 +1000

    Add placeholders for language and literacy page

--- src/app/components/language-and-literacy/language-and-literacy.ts
+++ src/app/components/language-and-literacy/language-and-literacy.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { YoutubePlayerModule } from '../shared/youtube-player/youtube-player.module';
 
 @Component({
   selector: 'app-language-and-literacy',
-  imports: [],
+  imports: [YoutubePlayerModule],
   templateUrl: './language-and-literacy.html',
   styleUrl: './language-and-literacy.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
