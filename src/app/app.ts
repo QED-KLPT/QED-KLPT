c39ee035a0347f653ecacb2f7fa7afab0e13f5b5
Matt Warwick <mattyboisterous@gmail.com>
2026-04-21 13:07:23 +1000

    SWA in place

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 import { Footer } from './_layout/footer/footer';
 import { Header } from './_layout/header/header';
 
@@ -10,4 +13,44 @@ import { Header } from './_layout/header/header';
   styleUrl: './app.scss',
 })
 export class App {
+  private readonly swUpdate = inject(SwUpdate);
+  private readonly destroyRef = inject(DestroyRef);
+
+  protected showUpdateNotice = false;
+  protected isRefreshing = false;
+
+  constructor() {
+    if (!this.swUpdate.isEnabled) {
+      return;
+    }
+
+    this.swUpdate.versionUpdates
+      .pipe(
+        filter((event): event is VersionReadyEvent => event.type === 'VERSION_READY'),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe(() => {
+        this.showUpdateNotice = true;
+      });
+  }
+
+  protected dismissUpdateNotice(): void {
+    this.showUpdateNotice = false;
+  }
+
+  protected async refreshForUpdate(): Promise<void> {
+    if (this.isRefreshing) {
+      return;
+    }
+
+    this.isRefreshing = true;
+
+    try {
+      await this.swUpdate.activateUpdate();
+      document.location.reload();
+    } catch (error) {
+      console.error('Unable to activate site update.', error);
+      this.isRefreshing = false;
+    }
+  }
 }
