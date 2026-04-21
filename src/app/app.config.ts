6e5f2bef7744f9debfd842c39211ea6854270b86
AMATYA <Jeny.AMATYA@qed.qld.gov.au>
2026-04-21 17:02:25 +1000

    Added a video content service, loaded transcript from txt files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideRouter } from '@angular/router';
 
@@ -7,6 +8,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideRouter(routes),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
--- src/app/components/language-and-literacy/language-and-literacy.ts
+++ src/app/components/language-and-literacy/language-and-literacy.ts
@@ -1,19 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { YoutubePlayerModule } from '../shared/youtube-player/youtube-player.module';
-
-type Video = {
-  title: string;
-  description: string;
-  youtubeUrl: string;
-  transcript: string[];
-};
-
-type Column = {
-  heading: string;
-  intro: string;
-  videos: Video[];
-};
+import { KlptVideoContentService, PageVideoColumn } from '../../services/klpt-video-content.service';
 
 @Component({
   selector: 'app-language-and-literacy',
@@ -23,42 +12,9 @@ type Column = {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LanguageAndLiteracy {
-    protected readonly videoColumns: Column[] = [
-    {
-      heading: 'Home Languages',
-      intro:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula, nibh vel sagittis hendrerit, lectus turpis volutpat nisl, et volutpat est erat non purus.',
-      videos: [
-        {
-          title: 'KLPT-home languages',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at sapien non purus fermentum dignissim.',
-          youtubeUrl: 'https://www.youtube.com/watch?v=XneIolMk42s',
-          transcript: [
-            'Educator: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'Narrator: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            'Educator: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-          ]
-        }        
-      ]
-    },
-    {
-      heading: 'Language and Literacy',
-      intro:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
-      videos: [
-        {
-          title: 'KLPT-learning areas',
-          description:
-            'Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper.',
-          youtubeUrl: 'https://www.youtube.com/watch?v=oyQmlHPgKPE',
-          transcript: [
-            'Narrator: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'Educator: Morbi leo risus, porta ac consectetur ac, vestibulum at eros.',
-            'Narrator: Vestibulum id ligula porta felis euismod semper.',
-          ]
-        }
-      ]
-    }
-  ];
+  protected readonly videoColumns$: Observable<PageVideoColumn[]>;
+
+  constructor(private readonly videoContentService: KlptVideoContentService) {
+    this.videoColumns$ = this.videoContentService.getPageColumns('language-and-literacy');
+  }
 }
