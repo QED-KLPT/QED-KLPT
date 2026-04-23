184cbd594f9e51eadc58d8896522450e4ca0679f
Andrew McGrath <andrew@sitestudio.co>
2026-04-23 12:55:11 +1000

    initial cta shared component commit

--- src/app/components/testing/testing.ts
+++ src/app/components/testing/testing.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CtaLinkComponent } from '../shared/cta-link/cta-link.component';
 
 @Component({
   selector: 'app-testing',
-  imports: [RouterLink],
+  imports: [RouterLink, CtaLinkComponent],
   templateUrl: './testing.html',
   styleUrl: './testing.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
