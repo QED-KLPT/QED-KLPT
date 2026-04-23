9a747d8d2096c991e0f00520f50c906174fba486
Andrew McGrath <andrew@sitestudio.co>
2026-04-23 14:18:51 +1000

    initial commit for direction-link

---
+++ src/app/components/testing-direction-links/testing-direction-links.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DirectionLinkComponent } from '../shared/direction-link/direction-link.component';
+
+@Component({
+  selector: 'app-testing-direction-links',
+  imports: [DirectionLinkComponent],
+  templateUrl: './testing-direction-links.html',
+  styleUrl: './testing-direction-links.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TestingDirectionLinks {}
