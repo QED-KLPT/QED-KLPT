704ded7b794339bc1721760f6d07951ee10d2d8e
Andrew McGrath <andrew@sitestudio.co>
2026-04-23 13:03:39 +1000

    update testing buttons

--- src/app/components/testing/testing.ts
+++ src/app/components/testing/testing.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CtaLinkComponent } from '../shared/cta-link/cta-link.component';
 
 @Component({
   selector: 'app-testing',
-  imports: [RouterLink, CtaLinkComponent],
+  imports: [RouterLink],
   templateUrl: './testing.html',
   styleUrl: './testing.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
