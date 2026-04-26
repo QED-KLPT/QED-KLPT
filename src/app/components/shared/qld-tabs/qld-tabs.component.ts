2f205815ddbda94487d6c6d6f987412e418956bb
Andrew McGrath <andrew@sitestudio.co>
2026-04-27 09:27:00 +1000

    initial commit for tabs shared component

---
+++ src/app/components/shared/qld-tabs/qld-tabs.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qld-tabs',
+  imports: [],
+  templateUrl: './qld-tabs.component.html',
+  styleUrl: './qld-tabs.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class QldTabsComponent {}
