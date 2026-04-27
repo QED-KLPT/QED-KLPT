550c6a0a6c9d9d3ad26ba0081a60bbeb326f8ad5
Andrew McGrath <andrew@sitestudio.co>
2026-04-27 12:20:06 +1000

    lorum ipsum for KLPT tool page

--- src/app/components/klpt-tool/klpt-tool.ts
+++ src/app/components/klpt-tool/klpt-tool.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { YoutubePlayerModule } from '../shared/youtube-player/youtube-player.module';
 
 @Component({
   selector: 'app-klpt-tool',
-  imports: [],
+  imports: [RouterLink, YoutubePlayerModule],
   templateUrl: './klpt-tool.html',
   styleUrl: './klpt-tool.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
