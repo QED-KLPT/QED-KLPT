3924479de8a6c4369db4f025139b7d6032f52100
Matt Warwick <mattyboisterous@gmail.com>
2026-04-30 12:20:56 +1000

    Dedicated styles for tool

--- src/app/components/klpt/klpt.ts
+++ src/app/components/klpt/klpt.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,5 +7,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './klpt.html',
   styleUrl: './klpt.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class Klpt {}
