fbefeb576e8d0e429221f99ce11f16432c24183a
AMATYA <Jeny.AMATYA@qed.qld.gov.au>
2026-05-06 11:34:28 +1000

    Updated video area

--- src/app/components/analysing-data/analysing-data.ts
+++ src/app/components/analysing-data/analysing-data.ts
@@ -38,7 +38,7 @@ export class AnalysingData implements OnInit {
         {
           title: 'Pattern spotting in everyday routines',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at sapien non purus fermentum dignissim.',
+            '',
           youtubeUrl: 'https://www.youtube.com/watch?v=RCmiHUNHa8c',
           transcript: [
             'Educator: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
@@ -49,7 +49,7 @@ export class AnalysingData implements OnInit {
         {
           title: 'Connecting notes to next steps',
           description:
-            'Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui.',
+            '',
           youtubeUrl: 'https://www.youtube.com/watch?v=t_TQXaHyjZM',
           transcript: [
             'Narrator: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.',
@@ -60,7 +60,7 @@ export class AnalysingData implements OnInit {
         {
           title: 'Interpreting trends across the week',
           description:
-            'Etiam porta sem malesuada magna mollis euismod. Maecenas faucibus mollis interdum.',
+            '',
           youtubeUrl: 'https://www.youtube.com/watch?v=Rg_Bk8mhsQI',
           transcript: [
             'Educator: Aenean lacinia bibendum nulla sed consectetur.',
@@ -73,41 +73,19 @@ export class AnalysingData implements OnInit {
     {
       heading: 'Planning responses',
       intro:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
+        '',
       videos: [
         {
           title: 'Analysing group participation',
           description:
-            'Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper.',
+            '',
           youtubeUrl: 'https://www.youtube.com/watch?v=1BbO3pI1GVk',
           transcript: [
             'Narrator: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
             'Educator: Morbi leo risus, porta ac consectetur ac, vestibulum at eros.',
             'Narrator: Vestibulum id ligula porta felis euismod semper.',
           ],
-        },
-        {
-          title: 'Using evidence in reflective conversations',
-          description:
-            'Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac.',
-          youtubeUrl: 'https://www.youtube.com/watch?v=XneIolMk42s',
-          transcript: [
-            'Educator: Donec sed odio dui. Cras justo odio, dapibus ac facilisis in.',
-            'Narrator: Maecenas sed diam eget risus varius blandit sit amet non magna.',
-            'Educator: Integer posuere erat a ante venenatis dapibus.',
-          ],
-        },
-        {
-          title: 'Turning insights into intentional teaching',
-          description:
-            'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.',
-          youtubeUrl: 'https://www.youtube.com/watch?v=oyQmlHPgKPE',
-          transcript: [
-            'Narrator: Sed posuere consectetur est at lobortis.',
-            'Educator: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'Narrator: Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.',
-          ],
-        },
+        }
       ],
     },
   ];
