f5dabb2ea70f3f95543547e0d0b9c492a2543f07
Andrew McGrath <andrew@sitestudio.co>
2026-05-18 14:08:24 +1000

    change LEARNER to CHILD for pdf

--- src/app/services/klpt-pdf-generator.service.spec.ts
+++ src/app/services/klpt-pdf-generator.service.spec.ts
@@ -66,8 +66,8 @@ describe('KlptPdfGeneratorService', () => {
   });
 
   describe('getFieldLabel', () => {
-    it('should map child-name to Learner Name', () => {
-      expect(service.getFieldLabel('child-name')).toBe('Learner Name');
+    it('should map child-name to Child name', () => {
+      expect(service.getFieldLabel('child-name')).toBe('Child name');
     });
 
     it('should map date to Date', () => {
