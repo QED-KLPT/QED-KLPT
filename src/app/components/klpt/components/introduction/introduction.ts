5fdf04099f1eab0b17c6eb5bb7cb7b130e935c61
AMATYA <Jeny.AMATYA@qed.qld.gov.au>
2026-06-01 16:36:23 +1000

    Updated klpt tool page

--- src/app/components/klpt/components/introduction/introduction.ts
+++ src/app/components/klpt/components/introduction/introduction.ts
@@ -1,11 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { YoutubePlayerModule } from '../../../shared/youtube-player/youtube-player.module';
+import { ViewportScroller } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-introduction',
-  imports: [RouterLink],
+  imports: [RouterLink, YoutubePlayerModule],
   templateUrl: './introduction.html',
   styleUrl: './introduction.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Introduction {}
+export class Introduction implements OnInit {
+  usingKlptTranscript: string = '';
+
+  constructor(
+    private scroll: ViewportScroller,
+    private http: HttpClient,
+  ) 
+  {    
+    this.http.get('assets/content/transcripts/home/introduction-to-the-klpt.txt', { responseType: 'text' }).subscribe(t => this.usingKlptTranscript = t);
+  }
+
+   ngOnInit(): void {
+    this.scroll.scrollToPosition([0, 0]);
+  }
+  
+}
