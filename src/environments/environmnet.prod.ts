a6e9ff8b59482efc6d72d732ec50dad2a08650e3
Matt Warwick <mattyboisterous@gmail.com>
2026-06-12 08:51:36 +1000

    POC vid player - platform agnostic - accepts passkey

--- src/environments/environmnet.prod.ts
+++ src/environments/environmnet.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  sessionExpiryDays: 14
+  sessionExpiryDays: 14,
+  videoAccessUrl: 'https://klpt-functions-gadka4gkawahejgx.australiasoutheast-01.azurewebsites.net/api/videos/access',
 };
