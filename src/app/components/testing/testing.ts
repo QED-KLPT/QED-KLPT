b6b6dd70a26de8ecd452b18a2aa104200460be33
Matt Warwick <mattyboisterous@gmail.com>
2026-06-12 09:51:54 +1000

    OK - test host a test vid on the testing page

--- src/app/components/testing/testing.ts
+++ src/app/components/testing/testing.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { VidPlayerModule } from '../shared/vid-player/vid-player.module';
+
 @Component({
   selector: 'app-testing',
-  imports: [RouterLink],
+  imports: [RouterLink, VidPlayerModule],
   templateUrl: './testing.html',
   styleUrl: './testing.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
