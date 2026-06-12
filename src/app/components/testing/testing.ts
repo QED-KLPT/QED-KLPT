8dd1ade697bfad035f58cc2a9e8b141d368da066
Matt Warwick <mattyboisterous@gmail.com>
2026-06-12 12:49:11 +1000

    Add button to flush SAS tokens

--- src/app/components/testing/testing.ts
+++ src/app/components/testing/testing.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { VideoAccessService } from '../../services/video-access.service';
 import { VidPlayerModule } from '../shared/vid-player/vid-player.module';
 
 @Component({
@@ -10,4 +11,17 @@ import { VidPlayerModule } from '../shared/vid-player/vid-player.module';
   styleUrl: './testing.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Testing {}
+export class Testing {
+  protected readonly showTestVideo = signal(true);
+  protected readonly videoAccessCleared = signal(false);
+
+  private readonly videoAccess = inject(VideoAccessService);
+
+  protected clearVideoAccess(): void {
+    this.videoAccess.clearAccessToken();
+    this.showTestVideo.set(false);
+    this.videoAccessCleared.set(true);
+
+    setTimeout(() => this.showTestVideo.set(true));
+  }
+}
