e89e4f92305b7f4d2ab46f6e6436beb8de761d4f
Matt Warwick <mattyboisterous@gmail.com>
2026-06-15 13:56:56 +1000

    OK - header links

--- src/app/_layout/header/header.ts
+++ src/app/_layout/header/header.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { GlobalAlertComponent } from '../../components/shared/global-alert';
 import { Nav } from '../nav/nav';
 
 @Component({
   selector: 'app-header',
-  imports: [GlobalAlertComponent, Nav],
+  imports: [GlobalAlertComponent, Nav, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
