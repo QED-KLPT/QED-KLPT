081f1b949293f7a0fe21fddca157942e7dbef92c
Matt Warwick <mattyboisterous@gmail.com>
2026-06-15 11:41:50 +1000

    OK - first pass

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  showDraftContentDisclaimer: true,
   sessionExpiryDays: 14,
   videoAccessUrl: 'http://localhost:7169/api/videos/access',
 };
--- src/environments/environmnet.prod.ts
+++ src/environments/environmnet.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  showDraftContentDisclaimer: true,
   sessionExpiryDays: 14,
   videoAccessUrl: 'https://klpt-functions-gadka4gkawahejgx.australiasoutheast-01.azurewebsites.net/api/videos/access',
 };
