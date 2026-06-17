e51943e95cbd80d4b26941c56ab3ce5dbf800c71
Matt Warwick <mattyboisterous@gmail.com>
2026-06-17 15:51:05 +1000

    Change again!

--- src/app/navigation/site-navigation.ts
+++ src/app/navigation/site-navigation.ts
@@ -49,7 +49,7 @@ export const SITE_NAV_ITEMS: SiteNavItem[] = [
     ],
   },
   {
-    label: 'KLPT learning observation tool',
+    label: 'Learning observation tool',
     path: '/klpt-learning-observation-tool',
     sideNav: false,
   },
