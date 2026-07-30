8857f3a3bfdd71451079ae3090b8e59b507964f0
Matt Warwick <mattyboisterous@gmail.com>
2026-07-30 11:52:15 +1000

    dev changes to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   showDraftContentDisclaimer: true,
   sessionExpiryDays: 14,
-  videoAccessUrl: 'http://localhost:7169/api/videos/access',
-  siteAccessUrl: 'http://localhost:7169/api/site/access',
+  videoAccessUrl: 'https://klpt-functions-dev-fehdg7emdrh0fsdg.australiasoutheast-01.azurewebsites.net/api/videos/access',
+  siteAccessUrl: 'https://klpt-functions-dev-fehdg7emdrh0fsdg.australiasoutheast-01.azurewebsites.net/api/site/access',
 };
